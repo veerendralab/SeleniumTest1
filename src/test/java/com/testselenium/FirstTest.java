fe631968af6c38445c947190e76083832f50d5b5
Mankani@30 <veerendra.hm@gmail.com>
2025-09-13 12:06:00 +0530

    chagnes to logs

--- src/test/java/com/testselenium/FirstTest.java
+++ src/test/java/com/testselenium/FirstTest.java
@@ -21,21 +21,27 @@ public class FirstTest {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.automationexercise.com/login"); // Replace with your login URL
+        driver.get("https://www.automationexercise.com/login"); 
+        System.out.println("Opening URL ...");
     }
 
     @Test
     public void testLogin() {
         WebElement usernameField = driver.findElement(By.name("email")); // Adjust locator as needed
+        
         WebElement passwordField = driver.findElement(By.name("password"));
         WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
 
         usernameField.sendKeys("veerenhm@gmail.com");
+        System.out.println("Entered User Name");
         passwordField.sendKeys("MyPassword@30");
+        System.out.println("Entered Password");
         loginButton.click();
+        System.out.println("Clicked on Login Button");
         
         String title = driver.getTitle();
         assertEquals(title, "Automation Exercise");
+        System.out.println("Validated Title...");
     }
 
     @AfterClass
